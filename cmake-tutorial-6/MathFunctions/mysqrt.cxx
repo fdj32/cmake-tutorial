e4a15dc5fe6b4519e9ed42f4c068293c23385659
Nick Feng <Nick.Feng@activenetwork.com>
2016-09-27 14:29:37 +0800

    questions about ChechFunctionExists and CheckIncludeFiles 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

--- cmake-tutorial-6/MathFunctions/mysqrt.cxx
+++ cmake-tutorial-6/MathFunctions/mysqrt.cxx
@@ -1,10 +1,6 @@
 #include <MathFunctions.h>
 
 double mysqrt(double x) {
-	// if we have both log and exp then use them
-	#if defined (HAVE_LOG) && defined (HAVE_EXP)
-		return exp(log(x)*0.5);
-	#endif // otherwise use an iterative approach
-	return sqrt(x);
+	return exp(log(x)*0.5)+1000;
 }
 
