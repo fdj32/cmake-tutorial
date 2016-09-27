a28241266d0a8f90e259efbb7b76b2e0f87f14fb
Nick Feng <Nick.Feng@activenetwork.com>
2016-09-27 15:22:48 +0800

    Adding a Generated File and Generator (Step 5)

---
+++ cmake-tutorial-7/clean.sh
@@ -0,0 +1,17 @@
+rm -rf CMakeFiles
+rm -rf Testing
+rm CMakeCache.txt
+rm install_manifest.txt
+rm *.cmake
+rm *.tcl
+rm Makefile
+rm Tutorial
+rm TutorialConfig.h
+
+rm -rf MathFunctions/CMakeFiles
+rm MathFunctions/*.cmake
+rm MathFunctions/libMathFunctions.a
+rm MathFunctions/Makefile
+
+rm MathFunctions/MakeTable
+rm MathFunctions/Table.h
